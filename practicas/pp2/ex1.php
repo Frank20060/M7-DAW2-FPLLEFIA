f809d2258ec6d012162c2661cbea0f99282bec8f
Frank20060 <159519815+Frank20060@users.noreply.github.com>
2025-09-19 16:30:39 +0000

    ej2tablas

---
+++ practicas/pp2/ex1.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 1</title>
+    <style>
+        body{
+            justify-items: center;
+            justify-content: center;
+            padding: 20px;
+            padding-left: 50px;
+            padding-right: 50px;
+            background-color: beige;
+            font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+            
+
+        }
+        h1{
+            color: brown;
+            font-size: 40px;
+        }
+        #numBox{
+            display: flex;
+            flex-wrap: wrap;
+            gap: 10px;  
+            width: 100%;  
+            height: fit-content;
+            border: 1px, dotted, brown;
+            padding: 5px;
+            font-size: 1.5em;
+            
+
+        }
+
+    </style>
+</head>
+<body>
+    <h1>Numeros pares entre 50 y 500</h1>
+    <div id="numBox">
+        <?php
+
+            for($num = 50; $num <=500; $num+= 2){
+
+                echo "<div>$num</div>";
+
+            }
+        
+        ?>
+    </div>
+</body>
+</html>
---
+++ practicas/pp2/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portada pp2</title>
+    <style>
+        body{
+            justify-items: center;
+            justify-content: center;
+            padding: 20px;
+            padding-left: 50px;
+            padding-right: 50px;
+            background-color: beige;
+            font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+            
+
+        }
+        h1{
+            color: brown;
+            font-size: 40px;
+        }
+    </style>
+</head>
+<body>
+    <h1>Practica 2</h1>
+    <h2><a href="ex1.php">Ejercicio 1</a></h2>
+    
+    <h2><a href="ex2.php">Ejercicio 2</a></h2>
+    
+</body>
+</html>
