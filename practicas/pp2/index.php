629127bb2b46b1f5fb4afa3d05784457b619117f
Frank20060 <159519815+Frank20060@users.noreply.github.com>
2025-09-22 14:46:47 +0000

    PP2B comenzando y retoques pp2

--- practicas/pp2/index.php
+++ practicas/pp2/index.php
@@ -30,12 +30,12 @@
 </head>
 <body>
     <h1>Practica 2</h1>
-    <h2><a href="ex1.php">Ejercicio 1</a></h2>
-    <h2><a href="ex2.php">Ejercicio 2</a></h2>
-    <h2><a href="ex3.php">Ejercicio 3</a></h2>
+    <h2><a href="ex1.php">Ejercicio 1 - Pares 50-500</a></h2>
+    <h2><a href="ex2.php">Ejercicio 2 - Tablas Multiplicar</a></h2>
+    <h2><a href="ex3.php">Ejercicio 3 - Num apeatorio Par o Impar</a></h2>
     <h2>Extras PP2</h2>
-    <h2><a href="ex4.php">Ejercicio Extra 1</a></h2>
-    <h2><a href="ex5.php">Ejercicio Extra 2</a></h2>
+    <h2><a href="ex4.php">Ejercicio Extra 1 - Numeros primos</a></h2>
+    <h2><a href="ex5.php">Ejercicio Extra 2 - Temperaturas</a></h2>
     
 </body>
 </html>
---
+++ practicas/pp2b/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Practica 2b</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet">
+    <style>
+        body{
+            justify-items: center;
+            justify-content: center;
+            padding: 20px;
+            padding-left: 50px;
+            padding-right: 50px;
+            background-color: grey;
+            font-family: "Nunito", sans-serif;
+
+            
+
+        }
+        h1{
+            color: white;
+            font-size: 40px;
+            text-shadow: 2px 2px 5px black;
+        }
+        a{
+
+            color: lightblue;
+            text-shadow: 2px 2px 5px black;
+        }
+        
+    </style>
+</head>
+<body>
+    <h1>Practica 2b Extras</h1>
+    <h2><a href="ex1.php">Ejercicio 1 - Lista de pares 50 - 500 clasificados</a></h2>
+    <h2><a href="ex2.php">Ejercicio 2 - Analisis de valores aleatorios</a></h2>
+</body>
+</html>
