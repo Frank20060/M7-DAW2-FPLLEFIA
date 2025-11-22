215de5a55172c3549f4e81a2a5d327d649ae1c29
Frank20060 <159519815+Frank20060@users.noreply.github.com>
2025-11-22 19:17:03 +0000

    CREO QUE ESTAN LAS CONSULTAS usando funciones

--- gymWeb/theme/db/consultas/dbComentarios.php
+++ gymWeb/theme/db/consultas/dbComentarios.php
@@ -1,9 +1,7 @@
 <?php
 include_once __DIR__ . "/../dbconfig/config.php";
 
-///Consulta para obtener los comentarios de una noticia junto con la información del usuario que los hizo
-
-
+///Comentario de noticia
 function getComentariosNoticia($id) {
     global $mysqli;
 
@@ -26,4 +24,33 @@ function getComentariosNoticia($id) {
     $stmt->close();
     return $res;
 }
+
+//Comentario de comentarioç
+
+
+function getComentariosComentario($id) {
+    global $mysqli;
+
+    $consulta =
+    '
+    SELECT c.id, c.comentario, c.fecha, c.id_respuesta, u.id AS id_user, u.nombre, u.apellido, u.imagen
+    FROM COMENTARIOS c
+    JOIN USUARIOS u ON c.id_usuario = u.id
+    WHERE c.id_respuesta = ?
+    ORDER BY c.fecha
+    ';
+
+    $stmt = $mysqli->prepare($consulta);
+
+    if (!$stmt) {
+        die("Error en prepare: " . $mysqli->error);
+    }
+
+    $stmt->bind_param("i", $id);
+    $stmt->execute();
+    $res = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+    $stmt->close();
+    return $res;
+}
+
 
